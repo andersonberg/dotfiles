916ecf39fb94f86ee935d933976f1a0e9454fc25
Anderson Berg <andersonberg@gmail.com>
2017-09-02 16:19:09 -0300

    update

--- setup.sh
+++ setup.sh
@@ -1,28 +1,28 @@
 if [ -f "ssh/config" ]; then
-	ln -s /home/andersonberg/Projetos/dotfiles/ssh/config ~/.ssh/config
+	ln -s /home/anderson/Projetos/dotfiles/ssh/config ~/.ssh/config
 fi
 
 if [ -f ".bashrc" ]; then	
-	ln -s /home/andersonberg/Projetos/dotfiles/.bashrc ~/.bashrc
+	ln -s /home/anderson/Projetos/dotfiles/.bashrc ~/.bashrc
 fi
 
 if [ -f ".gitconfig" ]; then
-	ln -s /home/andersonberg/Projetos/dotfiles/.gitconfig ~/.gitconfig
+	ln -s /home/anderson/Projetos/dotfiles/.gitconfig ~/.gitconfig
 fi
 
 
 if [ -f ".hgrc" ]; then
-	ln -s /home/andersonberg/Projetos/dotfiles/.hgrc ~/.hgrc
+	ln -s /home/anderson/Projetos/dotfiles/.hgrc ~/.hgrc
 fi
 
 if [ -f ".vimrc" ]; then
-	ln -s /home/andersonberg/Projetos/dotfiles/.vimrc ~/.vimrc
+	ln -s /home/anderson/Projetos/dotfiles/.vimrc ~/.vimrc
 fi
 
-if [ -f ".yaourtrc" ]; then
-	ln -s /home/andersonberg/Projetos/dotfiles/.yaourtrc ~/.yaourtrc
-fi
+#if [ -f ".yaourtrc" ]; then
+#	ln -s /home/andersonberg/Projetos/dotfiles/.yaourtrc ~/.yaourtrc
+#fi
 
-ln -s /home/andersonberg/Projetos/dotfiles/atom/config.cson ~/.atom/config.cson
+#ln -s /home/anderson/Projetos/dotfiles/atom/config.cson ~/.atom/config.cson
 
-apm install --packages-file /home/andersonberg/Projetos/dotfiles/atom/atom_packages.txt
+#apm install --packages-file /home/anderson/Projetos/dotfiles/atom/atom_packages.txt
